9830bf2977398ad9d98e6cb88804b2054c4142b6
shsa <shilovsa@gmail.com>
2020-06-16 17:41:25 +0700

    .

--- Kaleidoscope/Assets/Scripts/SpriteKaleidoscope.cs
+++ Kaleidoscope/Assets/Scripts/SpriteKaleidoscope.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
-[ExecuteInEditMode]
+//[ExecuteInEditMode]
 public class SpriteKaleidoscope : MonoBehaviour
 {
+    static Dictionary<Vector3Int, TriangleComponent> cache = new Dictionary<Vector3Int, TriangleComponent>();
+
     Texture2D _texture;
     public Texture2D texture;
     public int Size = 4;
@@ -21,6 +24,7 @@ public class SpriteKaleidoscope : MonoBehaviour
     Vector2 a;
     Vector2 b;
     Vector2 c;
+    int idx = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -67,23 +71,154 @@ public class SpriteKaleidoscope : MonoBehaviour
         var sr = obj.AddComponent<SpriteRenderer>();
         sr.sprite = _sprite;
         obj.transform.SetParent(transform);
-        obj.transform.localPosition = Vector3.zero;
-        obj.transform.localScale = Vector3.one;
+        var tri = obj.AddComponent<TriangleComponent>();
+        tri.center = Vector2.zero - a;
+        tri.rotation = Vector3Int.zero;
+        tri.index = Vector3Int.zero;
+        cache.Add(tri.index, tri);
+        idx = 0;
+        UpdateObject(tri);
 
-        var obj1 = Object.Instantiate(obj, transform);
-        obj1.transform.localPosition = Quaternion.Euler(0, 0, -60) * a;
-        var r = new Vector3(0, -180, -60);
-        
-        obj1.transform.localRotation = Quaternion.Euler(r);
-        obj1.transform.localScale = Vector3.one;
-        obj1.name = "t";
+        var stack = new Stack<TriangleComponent>();
+        stack.Push(tri);
 
-        obj1 = Object.Instantiate(obj, transform);
-        obj1.transform.localPosition = Quaternion.Euler(0, 0, -60) * a;
-        r += new Vector3(0, -180, -60);
-        obj1.transform.localRotation = Quaternion.Euler(r);
-        obj1.transform.localScale = Vector3.one;
-        obj1.name = "t";
+        var sqrH = (b - a).sqrMagnitude * 0.75f; // h = a * sqrt(3) / 2 => h^2 = a^2 * 3 / 4
+
+        var radius = 3;
+        var sqrRadius = radius * sqrH - sqrH / 2;
+        var coll = GetComponent<CircleCollider2D>();
+        coll.radius = Mathf.Sqrt(sqrRadius);
+        //CreateObject(stack, Vector2.zero, sqrRadius);
+        tri = CreateObject(stack, tri, Vector2Int.left, Vector2.zero, sqrRadius);
+        tri = CreateObject(stack, tri, Vector2Int.left, Vector2.zero, sqrRadius);
+        tri = CreateObject(stack, tri, Vector2Int.left, Vector2.zero, sqrRadius);
+        tri = CreateObject(stack, tri, Vector2Int.up, Vector2.zero, sqrRadius);
+    }
+
+    void UpdateObject(TriangleComponent tri)
+    {
+        tri.transform.localScale = Vector3.one;
+        tri.transform.localPosition = tri.center;
+        tri.transform.localRotation = Quaternion.Euler(tri.rotation);
+    }
+
+    void CreateObject(Stack<TriangleComponent> stack, Vector2 center, float sqrRadius)
+    {
+        while (stack.Count > 0)
+        {
+            var tri = stack.Pop();
+            CreateObject(stack, tri, Vector2Int.up, center, sqrRadius);
+            CreateObject(stack, tri, Vector2Int.down, center, sqrRadius);
+            CreateObject(stack, tri, Vector2Int.left, center, sqrRadius);
+            CreateObject(stack, tri, Vector2Int.right, center, sqrRadius);
+        }
+    }
+
+    TriangleComponent CreateObject(Stack<TriangleComponent> stack, TriangleComponent start, Vector2Int dir, Vector2 center, float sqrRadius)
+    {
+        var tri0 = start.GetComponent<TriangleComponent>();
+        var top = a;
+        var c = Vector2.zero;
+        var r = Vector3Int.zero;
+        var index = tri0.index;
+        idx++;
+        var name = "t " + idx.ToString() + " ";
+        if (tri0.index.z == 0)
+        {
+            if (dir == Vector2Int.left)
+            {
+                c = tri0.center + (Vector2)(Quaternion.Euler(0, 0, 60) * top);
+                r = new Vector3Int(tri0.rotation.x, tri0.rotation.y - 180, tri0.rotation.z + 60);
+                index = new Vector3Int(index.x - 1, index.y, 1);
+                name += "left";
+            }
+            else
+            if (dir == Vector2Int.right)
+            {
+                c = tri0.center + (Vector2)(Quaternion.Euler(0, 0, -60) * top);
+                r = new Vector3Int(tri0.rotation.x, tri0.rotation.y - 180, tri0.rotation.z - 60);
+                index = new Vector3Int(index.x, index.y, 1);
+                name += "right";
+            }
+            else
+            if (dir == Vector2Int.down)
+            {
+                c = tri0.center + (Vector2)(Quaternion.Euler(0, 0, 180) * top);
+                r = new Vector3Int(tri0.rotation.x - 180, tri0.rotation.y, tri0.rotation.z);
+                index = new Vector3Int(index.x, index.y - 1, 1);
+                name += "down";
+            }
+            else
+            {
+                return null;
+            }
+        }
+        else
+        {
+            if (dir == Vector2Int.left)
+            {
+                c = tri0.center + (Vector2)(Quaternion.Euler(0, 0, 120) * top);
+                r = new Vector3Int(tri0.rotation.x, tri0.rotation.y - 180, tri0.rotation.z + 60);
+                index = new Vector3Int(index.x, index.y, 0);
+                name += "left";
+            }
+            else
+            if (dir == Vector2Int.right)
+            {
+                c = tri0.center + (Vector2)(Quaternion.Euler(0, 0, -120) * top);
+                r = new Vector3Int(tri0.rotation.x, tri0.rotation.y - 180, tri0.rotation.z - 60);
+                index = new Vector3Int(index.x + 1, index.y, 0);
+                name += "right";
+            }
+            else
+            if (dir == Vector2Int.up)
+            {
+                c = tri0.center + top;
+                r = new Vector3Int(tri0.rotation.x - 180, tri0.rotation.y, tri0.rotation.z);
+                index = new Vector3Int(index.x, index.y + 1, 0);
+                name += "up";
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        if (cache.TryGetValue(index, out var tri))
+        {
+            return null;
+        }
+
+        var q = Quaternion.Euler(r);
+        Vector2 a0 = q * top;
+        Vector2 b0 = Quaternion.Euler(0, 0, 60) * a0;
+        Vector2 c0 = Quaternion.Euler(0, 0, 60) * b0;
+
+        if ((c + a0 - center).sqrMagnitude > sqrRadius && (c + b0 - center).sqrMagnitude > sqrRadius && (c + c0 - center).sqrMagnitude > sqrRadius)
+        {
+            return null;
+        }    
+
+        var obj = new GameObject(name);
+        var sr = obj.AddComponent<SpriteRenderer>();
+        sr.sprite = _sprite;
+        obj.transform.SetParent(transform);
+        tri = obj.AddComponent<TriangleComponent>();
+        tri.index = index;
+        tri.center = c;
+        tri.rotation = r;
+
+        UpdateObject(tri);
+
+        cache.Add(index, tri);
+        stack.Push(tri);
+
+        return tri;
+    }
+
+    void CreateObject(Vector3Int pos)
+    {
+
     }
 
     void Update()
---
+++ Kaleidoscope/Assets/Scripts/TriangleComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEngine;
+
+public enum TriangleType
+{
+    Up,
+    Down
+}
+
+public enum TriangleDir
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+public class TriangleComponent : MonoBehaviour
+{
+    public Vector3Int index;
+    public Vector2 center;
+    public Vector3Int rotation;
+}
