0f8d250f108da7198fa0b976f2abb6da0e46cd94
shsa <shilovsa@gmail.com>
2020-06-28 13:41:32 +0700

    Test3D: first init

---
+++ Test3D/Assets/Test10k/Scripts/Test10k.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test10k : MonoBehaviour
+{
+	public GameObject cubePrefab;
+	public float radius = 100;
+    public int count = 10000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10000; i++)
+		{
+			var cube = Instantiate(cubePrefab, Random.insideUnitSphere * radius, Quaternion.identity, transform);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
