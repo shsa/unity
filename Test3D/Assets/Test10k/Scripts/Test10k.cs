b2a1d8a2908e619d20d0d9c16d5d9c8a51888ba4
shsa <shilovsa@gmail.com>
2020-06-28 22:02:01 +0700

    Test3D.Test10k: driving camera

--- Test3D/Assets/Test10k/Scripts/Test10k.cs
+++ Test3D/Assets/Test10k/Scripts/Test10k.cs
@@ -7,6 +7,7 @@ public class Test10k : MonoBehaviour
 	public GameObject cubePrefab;
 	public float radius = 100;
     public int count = 10000;
+    public GameObject _camera;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,21 @@ public class Test10k : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Fire1"))
+        {
+            var h = Input.GetAxis("Horizontal");
+            var v = Input.GetAxis("Vertical");
+
+            _camera.transform.Rotate(Vector3.left, v);
+            _camera.transform.Rotate(Vector3.up, h);
+        }
+        else
+        {
+            var h = Input.GetAxis("Horizontal");
+            var v = Input.GetAxis("Vertical");
+
+            _camera.transform.Translate(Vector3.forward * v * 0.25f);
+            _camera.transform.Translate(Vector3.left * h * 0.25f);
+        }
     }
 }
